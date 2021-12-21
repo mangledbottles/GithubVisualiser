7523a00392f89b1139b8bb740fb35c1315cfda66
Dermot <contact@dermotobrien.me>
2021-12-21 20:55:19 +0000

    Import Navbar and create static content

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,48 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Navbar from "./Navbar.tsx";
+
+import Box from "@mui/material/Box";
 
 export default function Home() {
   return (
     <div>
-      <main>
-        Hey!
-      </main>
-
+      <Navbar />
+      <Box
+        sx={{
+          display: "flex",
+          "& > :not(style)": {
+            m: 2,
+            marginTop: 4,
+          },
+        }}
+      >
+        <Container maxWidth="sm">
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </div>
-  )
+  );
 }
