7415a327c4ae555cf6a2de986e039dc5856e3811
Dermot <contact@dermotobrien.me>
2021-12-22 19:47:45 +0000

    API: getCommitHistory

--- pages/api/Repositories.ts
+++ pages/api/Repositories.ts
@@ -1,7 +1,13 @@
-async function getRespositories(username: string) {
+async function getRespositories(username: String) {
     const response = await fetch(`https://api.github.com/users/${username}/repos`);
     const data = await response.json();
     return data;
 }
 
-export { getRespositories };
+async function getCommitHistory(username: String, repo: String) {
+    const response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+    const data = await response.json();
+    return data;
+}
+
+export { getRespositories, getCommitHistory };
