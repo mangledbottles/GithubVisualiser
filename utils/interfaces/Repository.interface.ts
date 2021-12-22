52eb3697eaeb5030436410b02af2e5096bd65a7b
Dermot <contact@dermotobrien.me>
2021-12-22 15:51:40 +0000

    Create Interfaces: Repositories, Repository

---
+++ utils/interfaces/Repository.interface.ts
@@ -0,0 +1,10 @@
+export interface Repositories {
+    repositories: Repository[]
+}
+
+export interface Repository {
+    id: number;
+    name: string;
+    html_url: string;
+    description: string;
+}
