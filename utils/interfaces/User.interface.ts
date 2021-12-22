38f1d554fc550645a60e3650ed6c644c36672188
Dermot <contact@dermotobrien.me>
2021-12-22 13:43:43 +0000

    Create User.interface.ts

---
+++ utils/interfaces/User.interface.ts
@@ -0,0 +1,11 @@
+export interface User {
+    username: string;
+    avatar_url: string;
+    bio: string;
+    created_at: string;
+    followers: number;
+    following: number;
+    location: string;
+    name: string;
+    public_repos: number;
+}
