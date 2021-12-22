865fbfe677e7fb74bbe190536b248e78c7e8967c
Dermot <contact@dermotobrien.me>
2021-12-22 13:45:02 +0000

    Manage state in Navbar and Profile - setter and getter

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -1,82 +1,88 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Menu from '@mui/material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import * as API from './api';
-import { useState } from 'react';
-import _ from 'lodash';
-// import * as API from './api';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import * as API from "./api";
+import { useState } from "react";
+import _ from "lodash";
+import Autocomplete from "@mui/material/Autocomplete";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+// Import TS Interfaces
+import { User } from "../utils/interfaces/User.interface";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-
-// Creating a custom hook
-function useInput(defaultValue) {
-  const [value, setValue] = useState(defaultValue);
-  const onChange = _.debounce(e => {
-    API.Users.getUser(e.target.value).then(res => { console.log(res) });
-    console.log({value: e.target.value});
-    // setValue(e.target.value || ' ')
-  }, 600);
-
-  return {
-    value,
-    onChange,
-    };
-}
-
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
 
-export default function PrimarySearchAppBar() {
+
+export default function PrimarySearchAppBar({ setUsername, setUser }) {
+  // Creating a custom hook
+  function useInput(defaultValue) {
+    const [value, setValue] = useState(defaultValue);
+
+    const onChange = _.debounce((e) => {
+      API.Users.getUser(e.target.value).then((res: User) => {
+        console.log(res);
+        setUser(res);
+      });
+      console.log({ value: e.target.value });
+      setUsername(e.target.value);
+      // setValue(e.target.value || ' ')
+    }, 600);
+
+    return {
+      value,
+      onChange,
+    };
+  }
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
@@ -87,46 +93,42 @@ export default function PrimarySearchAppBar() {
     handleMobileMenuClose();
   };
 
-
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
-    >
-    </Menu>
+    ></Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
-    >
-     
-    </Menu>
+    ></Menu>
   );
 
   const inputProps = useInput();
@@ -134,12 +136,11 @@ export default function PrimarySearchAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            sx={{ display: { xs: "none", sm: "block" } }}
           >
             Github Visualiser
           </Typography>
@@ -151,13 +152,11 @@ export default function PrimarySearchAppBar() {
             <StyledInputBase
               {...inputProps}
               placeholder="Github Username..."
-              inputProps={{ 'aria-label': 'search' }}
-              // value={inputProps.value}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
 
           <Box sx={{ flexGrow: 1 }} />
-
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
