9a8583459f6093a4045d6d6e26328dc2ef01cc2b
Dermot <contact@dermotobrien.me>
2021-12-23 23:48:35 +0000

    Add explanation to main page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 // Import components
 import Navbar from "./Navbar.tsx";
@@ -24,6 +25,11 @@ export default function Home() {
         setUser={setUser}
         setRepositories={setRepositories}
       />
+
+      <Typography variant="h4" component="h4">
+        Graph commits per day
+      </Typography>
+
       <Box
         sx={{
           display: "flex",
@@ -33,8 +39,8 @@ export default function Home() {
           },
         }}
       >
-        {/* Grid to take all available */}
-        <Grid sx={{ flexGrow: 0 }} width="100vw" container spacing={2}>
+        ;{/* Grid to take all available */}
+        <Grid sx={{ flexGrow: 0 }} width="100vw" container spacing={2} xs={12}>
           <Profile username={username} user={user} />
           <Repositories repositories={repositories} setCommits={setCommits} />
           <Graphs commits={commits} />
